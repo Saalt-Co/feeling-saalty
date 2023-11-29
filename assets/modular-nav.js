7b0918a9d3b31f1a2ccba72e9ee3e982d638ac53
Jacob Giordano <jacob.giordano@saalt.com>
2023-11-29 12:51:06 -0800

    [SHOP-281] All sorts of changes to Mod Nav to get it closer to working
    - Split out js to it's own file
    - Added new inputs for link blocks
    - Adjusted implementation to use anchors or buttons depending on intention on click

---
+++ assets/modular-nav.js
@@ -0,0 +1,99 @@
+class ModularNavBar extends HTMLElement {
+  constructor() {
+    super();
+
+    this.navClick = this.handleNavLinkClick.bind(this);
+    this.throttleDelay = 50;
+
+    window.addEventListener(
+      'resize',
+      throttle(() => {
+        // this.handleScroll();
+        console.log(window.width);
+      }, this.throttleDelay)
+    );
+  }
+
+  connectedCallback() {
+    this.addEventListeners();
+  }
+
+  disconnectedCallback() {
+    this.removeEventListeners();
+  }
+
+  addEventListeners() {
+    const thisNavLinks = this.querySelectorAll('a');
+    const thisNavBtns = this.querySelectorAll('button');
+    for (const link of thisNavLinks) {
+      link.addEventListener('click', this.navClick, true);
+    }
+    for (const button of thisNavBtns) {
+      button.addEventListener('click', this.navClick, true);
+    }
+  }
+
+  removeEventListeners() {
+    const thisNavLinks = this.querySelectorAll('a');
+    const thisNavBtns = this.querySelectorAll('button');
+    for (const link of thisNavLinks) {
+      link.removeEventListener('click', this.navClick, true);
+    }
+    for (const button of thisNavBtns) {
+      button.removeEventListener('click', this.navClick, true);
+    }
+  }
+
+  handleNavLinkClick(e) {
+    e.stopPropagation();
+    console.clear();
+    console.log(e.target);
+    this.querySelector('.active').classList.remove('active');
+    e.target.classList.add('active');
+    if (e.target.getAttribute('data-target-element')) {
+      this.scrollTo(document.querySelector(e.target.getAttribute('data-target-element')));
+    }
+  }
+
+  scrollTo(element) {
+    const headerInfo = this.getHeaderInfo();
+    console.clear();
+    console.log('——————————————————————————');
+    console.log(element);
+    console.log('——————————————————————————');
+    console.log(headerInfo);
+    console.log('——————————————————————————');
+    window.scroll({
+      left: 0,
+      top: element.getBoundingClientRect().top + window.scrollY + headerInfo.totalHeaderHeight,
+      behavior: 'smooth',
+    });
+  }
+
+  getHeaderInfo() {
+    let headerInfo = {};
+
+    let selectors = JSON.parse(this.querySelector('[id*="headerSelectors_"]').innerHTML);
+
+    headerInfo.mainNav = document.querySelector(selectors[0]);
+    headerInfo.announcementBar = document.querySelector(selectors[1]);
+
+    if (headerInfo.mainNav) {
+      headerInfo.mainNavHeight = headerInfo.mainNav.getBoundingClientRect().height;
+    } else {
+      headerInfo.mainNavHeight = 0;
+    }
+
+    if (headerInfo.announcementBar) {
+      headerInfo.announcementBarHeight = headerInfo.announcementBar.getBoundingClientRect().height;
+    } else {
+      headerInfo.announcementBarHeight = 0;
+    }
+
+    headerInfo.totalHeaderHeight = headerInfo.mainNavHeight + headerInfo.announcementBarHeight;
+
+    return headerInfo;
+  }
+}
+
+customElements.define('modular-nav-bar', ModularNavBar);
